22bf79d2a852f06fc236a65952f1322f9679624c
yangrulin <18801301274@163.com>
2020-04-10 18:46:34 +0800

    04/10-1

--- App.vue
+++ App.vue
@@ -1,4 +1,5 @@
 <script>
+	// https://www.imovietrailer.com/superhero/doc.html
 	export default {
 		// onLaunch: function() {
 		// 	console.log('App Launch')
@@ -14,4 +15,10 @@
 
 <style>
 	/*每个页面公共css */
+	.page{
+		width: 100%;
+		height: 100%;
+		background-color: #f7f7f7;
+		position: absolute;
+	}
 </style>
