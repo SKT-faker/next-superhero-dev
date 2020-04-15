2c56097ac4787dc0c7025bbf926aeb976bc82568
yangrulin <18801301274@163.com>
2020-04-15 18:18:27 +0800

    04/15

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,25 +1,42 @@
 <template>
 	<view class="page">
 		<swiper :indicator-dots="true" :autoplay="true" class="carousel">
-			<swiper-item>
-				<image src="../../static/carousel/batmanvssuperman.png" class="carousel"></image>
+			<swiper-item v-for="carousel in carouselList">
+				<image 
+					:src="carousel.image"
+					 class="carousel"></image>
 			</swiper-item>
-			<swiper-item>
+			<!-- <swiper-item>
 				<image src="../../static/carousel/spiderman.png" class="carousel"></image>
-			</swiper-item>
+			</swiper-item> -->
 		</swiper>
 	</view>
 </template>
 
 <script>
+	// import common from '../../common/common.js'
+	
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				carouselList: []
 			}
 		},
 		onLoad() {
-
+			let serverUrl = this.serverUrl;
+			
+			// 请求轮播图数据
+			uni.request({
+			    url: serverUrl + '/index/carousel/list?qq=2715705745', 
+				method: "POST",
+			    success: (res) => {
+			        console.log(res.data);
+			        
+					if(res.data.status == 200){
+						this.carouselList = res.data.data;
+					}
+			    }
+			});
 		},
 		methods: {
 
